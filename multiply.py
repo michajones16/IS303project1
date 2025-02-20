24ace8fde273c1d6f18553a71b1173dea51705c4
jakobkahler <jkahler@byu.edu>
2025-02-20 10:28:03 -0700

    Created a function to multiply two numbers
    Takes two numbers and multiplies those numbers

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Jakob Kahler
+# 2/20/2025
+# Take input and multiply it
+
+# Takes two numbers and returns the product
+def mutiplyTwoNumbers(iNum1,iNum2):
+    result = iNum1 * iNum2
+    return result
